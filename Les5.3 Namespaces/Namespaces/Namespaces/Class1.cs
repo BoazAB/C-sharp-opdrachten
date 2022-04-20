f2d582ac0cbc988559176019699a3463f546ce0f
Blauwberry <32714@ma-web.nl>
2022-04-20 19:46:47 +0200

    namespaces verduidelijken

--- Les5.3 Namespaces/Namespaces/Namespaces/Class1.cs
+++ Les5.3 Namespaces/Namespaces/Namespaces/Class1.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Namespace
+namespace Name1
 {
-    internal class Class1
+    public class Class1
     {
-        private int Kaketoe;
+        
+    }
+}
+namespace Name2
+{
+    public class Class2
+    {
 
-        public void Feed(int amound)
+    }
+}
+namespace Name3
+{
+    public class Class3
+    {
+        static void Main()
         {
-            Kaketoe -= amount;
-            Console.WriteLine(Kaketoe);
+            Name1.Class1 flurry = new Name1.Class1();
+            Name2.Class2 burger = new Name2.Class2();
         }
     }
-
-
 }
